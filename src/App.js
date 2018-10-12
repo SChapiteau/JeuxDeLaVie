3f1cd6576bbc0c047f1df351e3331a6b6d0f71ba
Stephane.C <tchadizman@gmail.com>
2018-10-13 00:26:03 +0200

    Mise en place d'un timer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Game from './Component/Game.js';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
--- src/Component/Game.js
+++ src/Component/Game.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
+import './Game.css';
 
 class Game extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {interation: 1};
+            
+      }
 
-  render() {
-    return (
-      <div className="App">
-        Game
-      </div>
-    );
-  }
+    render() {
+        return (
+        <div id="GameControls">
+            <div >
+            <button onClick={this.reset}>Reset</button>
+            <button onClick={this.start}>Start</button>
+            <label>Itération :</label>
+            <label className="iterationLabel"> {this.state.interation}</label>
+        </div>
+        </div>
+        );
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.loop(),
+            500
+          );
+    }
+  
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    start(){
+        this.setState({interation: 0});   
+        this.timerID = setInterval(
+            () => this.loop(),
+            500
+            );
+    }
+
+    loop = () => {
+        this.setState({interation: this.state.interation +1});        
+    }
+
+    reset = () => {
+        //interaction = 0;
+    }
+
 }
 
 export default Game;
