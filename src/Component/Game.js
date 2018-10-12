e9d6aca686c67459bd09ae8ab211a1aaa7f0225d
Stephane.C <tchadizman@gmail.com>
2018-10-12 19:29:44 +0200

    Commit initial

---
+++ src/Component/Game.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        Game
+      </div>
+    );
+  }
+}
+
+export default Game;
